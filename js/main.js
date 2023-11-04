6178602eeb4d5b822e4b0d665c875502afb0b90e
Kevin Martínez <kevinmartinezcto@gmail.com>
2023-11-04 10:46:46 -0300

    cosas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const nav = document.querySelector("#nav-bar");
 const abrir = document.querySelector("#burger-open");
 const cerrar = document.querySelector("#burger-close");
+const barraTransparente = document.querySelector(".container-open");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
+    barraTransparente.style.visibility = "hidden";
 })
 
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
+    barraTransparente.style.visibility = "visible";
 })
