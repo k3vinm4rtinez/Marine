8b4e5242087a552275a826e61c70c172d9b042b8
Kevin Martínez <kevinmartinezcto@gmail.com>
2023-11-18 12:58:43 -0300

    slider

--- assets/js/mySlider.js
+++ assets/js/mySlider.js
@@ -1,13 +1,14 @@
 const scrollContainer = document.querySelector(".myCards");
 const backBtn = document.querySelector("#backBtn");
 const nextBtn = document.querySelector("#nextBtn");
+const sliderHeight = scrollContainer.offsetHeight;
 
 nextBtn.addEventListener("click", () =>{
     scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollBy(0, 500);
+    scrollContainer.scrollBy(0, sliderHeight);
 });
 
 backBtn.addEventListener("click", () =>{
     scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollBy(0, -500);
+    scrollContainer.scrollBy(0, -sliderHeight);
 });
