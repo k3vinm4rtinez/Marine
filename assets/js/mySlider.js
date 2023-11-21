e9f023779a167d4d20d2dd27dee6c20e1489ae5e
Kevin Martínez <kevinmartinezcto@gmail.com>
2023-11-20 21:23:13 -0300

    cosas

--- assets/js/mySlider.js
+++ assets/js/mySlider.js
@@ -7,9 +7,18 @@ nextBtn.addEventListener("click", (evt) =>{
     evt.preventDefault();
     scrollContainer.style.scrollBehavior = "smooth";
     scrollContainer.scrollBy(0, sliderHeight);
+
+    if(scrollContainer.scrollBy(0, sliderHeight)){
+        nextBtn.disabled = true;
+    }
+
 });
 
 backBtn.addEventListener("click", () =>{
     scrollContainer.style.scrollBehavior = "smooth";
     scrollContainer.scrollBy(0, -sliderHeight);
+
+    if(scrollContainer.scrollBy(0, -sliderHeight)){
+        backBtn.disabled = true;
+    }
 });
