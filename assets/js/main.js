891b80c7fb3463b0d7b76160806e8ad23ce337f9
Kevin Martínez <kevinmartinezcto@gmail.com>
2023-12-22 21:03:52 -0300

    cosas

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,15 @@ cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
     barraTransparente.style.visibility = "visible";
 })
+
+const $form = document.getElementById('form');
+const $buttonMailto = document.getElementById('mimail');
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    $buttonMailto.setAttribute('href', `mailto:marinedesign.uy@gmail.com?subject=${form.get('name')} - ${form.get('email')} - ${form.get('phone')}&body=${form.get('message')}`)
+    $buttonMailto.click()
+}
